fdeb3e0af162cb39c56472ed76abfdd64fb6a108
CyndelHerolt <herolt.cyndel@gmail.com>
2024-02-22 10:14:49 +0100

    feat: affiche les dernières traces créées sur le dashboard

--- src/Twig/Components/TraceCard.php
+++ src/Twig/Components/TraceCard.php
@@ -2,9 +2,24 @@
 
 namespace App\Twig\Components;
 
+use App\Repository\TraceRepository;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
 final class TraceCard
 {
+    public int $id;
+
+    public function __construct(
+        protected TraceRepository $traceRepository,
+    )
+    {
+    }
+
+    public function getTrace()
+    {
+        $trace = $this->traceRepository->find($this->id);
+
+        return $trace;
+    }
 }

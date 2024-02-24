430ca2b27f683f23f08c3b7df67e1360ba323d60
CyndelHerolt <herolt.cyndel@gmail.com>
2024-02-24 11:41:49 +0100

    feat: trace lien preview auto

--- src/Twig/Components/TraceContent.php
+++ src/Twig/Components/TraceContent.php
@@ -39,13 +39,45 @@ final class TraceContent
 
         preg_match('/<title>(.*?)<\/title>/is', $html, $matches);
         $title = $matches[1] ?? '';
+        // si pas de titre, on met le nom du site
+        if (empty($title)) {
+            preg_match('/<meta property="og:site_name" content="(.*?)"/is', $html, $matches);
+            $title = $matches[1] ?? '';
+            // si pas de titre, on met le nom du site
+            if (empty($title)) {
+                $title = parse_url($url, PHP_URL_HOST);
+            }
+        }
+        // si pas de titre, on met un titre par défaut
+        if (empty($title)) {
+            $title = null;
+        }
 
         preg_match('/<meta name="description" content="(.*?)"/is', $html, $matches);
         $description = $matches[1] ?? '';
         $description = substr($description, 0, 75);
+        // si pas de description, on cherche une balise og:description
+        if (empty($description)) {
+            preg_match('/<meta property="og:description" content="(.*?)"/is', $html, $matches);
+            $description = $matches[1] ?? '';
+            $description = substr($description, 0, 75);
+        }
+        // si pas de description, on met une description par défaut
+        if (empty($description)) {
+            $description = null;
+        }
 
         preg_match('/<meta property="og:image" content="(.*?)"/is', $html, $matches);
         $image = $matches[1] ?? '';
+        // si pas d'image, on cherche une image dans le contenu
+        if (empty($image)) {
+            preg_match('/<img src="(.*?)"/is', $html, $matches);
+            $image = $matches[1] ?? '';
+            // si pas d'image, on met l'image link.jpg qui se trouve dans assets/images
+            if (empty($image)) {
+                $image = null;
+            }
+        }
 
         return [
             'title' => $title,

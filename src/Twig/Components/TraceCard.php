f68c52b9114952487a58ffaae7841f639d65104c
CyndelHerolt <herolt.cyndel@gmail.com>
2024-06-24 14:49:24 +0200

    wip: refactor - add trace from portfolio layout

--- src/Twig/Components/TraceCard.php
+++ src/Twig/Components/TraceCard.php
@@ -2,6 +2,7 @@
 
 namespace App\Twig\Components;
 
+use App\Components\Trace\TraceRegistry;
 use App\Repository\TraceRepository;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
@@ -10,8 +11,13 @@ final class TraceCard
 {
     public int $id;
 
+    public string $type = '';
+
+    public int $page;
+
     public function __construct(
         protected TraceRepository $traceRepository,
+        protected TraceRegistry $traceRegistry,
     )
     {
     }
@@ -19,6 +25,8 @@ final class TraceCard
     public function getTrace()
     {
         $trace = $this->traceRepository->find($this->id);
+        $type = $trace->getType();
+        $this->type = $this->traceRegistry->getTypeTrace($type)::TYPE;
 
         return $trace;
     }
